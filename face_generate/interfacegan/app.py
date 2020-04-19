573f1de20cc257ce8c001a1070cb703bb4ada163
Chris <chrisnakovski@gmail.com>
2020-04-19 02:36:59 -0700

    zipfile wrapping needs fix

--- face_generate/interfacegan/app.py
+++ face_generate/interfacegan/app.py
@@ -2,31 +2,21 @@ import os.path
 import cv2
 import numpy as np
 from tqdm import tqdm
-
+import json
+import sys
 from models.model_settings import MODEL_POOL
+from io import BytesIO
 from models.stylegan_generator import StyleGANGenerator
 from utils.logger import setup_logger
 from utils.manipulator import linear_interpolate
-from flask import Flask, flash, request, redirect, url_for
+from flask import Flask, flash, request, redirect, url_for, send_file
+from zipfile import ZipFile
 from werkzeug.utils import secure_filename
 
 ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
 img_size = 512
 
 app = Flask(__name__)
-
-cfg = {
-    'output_dir': 'results',
-    'model_name': 'stylegan_ffhq',
-}
-
-boundaries = {
-    'age': 'SpaceFace/face_generate/interfacegan/boundaries/stylegan_ffhq_age_w_boundary.npy',
-    'gender': 'SpaceFace/face_generate/interfacegan/boundaries/stylegan_ffhq_gender_w_boundary.npy',
-    'pose': 'SpaceFace/face_generate/interfacegan/boundaries/stylegan_ffhq_pose_w_boundary.npy',
-    'smile': 'SpaceFace/face_generate/interfacegan/boundaries/stylegan_ffhq_smile_w_boundary.npy',
-    'glasses': 'SpaceFace/face_generate/interfacegan/boundaries/stylegan_ffhq_eyeglasses_w_boundary.npy'
-}
 
 example_req_body = {
     'type': 'age',
@@ -38,22 +28,37 @@ example_req_body = {
 def setup():
     global logger
     global model
-    logger = setup_logger(cfg['output_dir'], logger_name='generate_data')
+    global boundaries
+    global cfg
+    global kwargs
+    cfg = {
+        'output_dir': 'results',
+        'model_name': 'stylegan_ffhq',
+    }
+    logger = setup_logger(logger_name='generate_data')
     model = StyleGANGenerator(cfg['model_name'], logger)
-
+    kwargs = {'latent_space_type': 'Wp'}
+    boundaries = {
+        'age': 'boundaries/stylegan_ffhq_age_w_boundary.npy',
+        'gender': 'boundaries/stylegan_ffhq_gender_w_boundary.npy',
+        'pose': 'boundaries/stylegan_ffhq_pose_w_boundary.npy',
+        'smile': 'boundaries/stylegan_ffhq_smile_w_boundary.npy',
+        'glasses': 'boundaries/stylegan_ffhq_eyeglasses_w_boundary.npy'
+    }
 
 def manipulate(boundary, min, max, steps):
     logger.info(f'Preparing boundary.')
 
-@app.route('/edit/<uuid:id>', methods=['POST'])
+@app.route('/edit/<id>', methods=['POST'])
 def edit_image(id):
     print (request.is_json)
-    content = request.get_json()
+    content = json.loads(request.data, strict=False)
+    print(content)
 
-    print (content)
-    if 'type' not in content:
+    if not content or 'type' not in content:
         return 'Valid type not found'
 
+    print(content)
     b = content['type']
 
     if b not in boundaries:
@@ -68,38 +73,43 @@ def edit_image(id):
     boundary = np.load(boundary_path)
 
     logger.info(f'Preparing latent codes.')
-    if os.path.isfile(args.input_latent_codes_path):
-      logger.info(f'  Load latent codes from `{args.input_latent_codes_path}`.')
-      latent_codes = np.load(args.input_latent_codes_path)
+    latent_code_path = f'latents/{id}.npy'
+    if os.path.isfile(latent_code_path):
+      logger.info(f'  Load latent codes from `{latent_code_path}`.')
+      latent_codes = np.load(latent_code_path)
       latent_codes = model.preprocess(latent_codes, **kwargs)
     else:
-      logger.info(f'  Sample latent codes randomly.')
-      latent_codes = model.easy_sample(args.num, **kwargs)
-    np.save(os.path.join(args.output_dir, 'latent_codes.npy'), latent_codes)
+      logger.info(f'  No latent codes found. Error')
+
     total_num = latent_codes.shape[0]
 
     logger.info(f'Editing {total_num} samples.')
-    for sample_id in tqdm(range(total_num), leave=False):
-      interpolations = linear_interpolate(latent_codes[sample_id:sample_id + 1],
-                                          boundary,
-                                          start_distance=args.start_distance,
-                                          end_distance=args.end_distance,
-                                          steps=args.steps)
-      interpolation_id = 0
-      for interpolations_batch in model.get_batch_inputs(interpolations):
-        if gan_type == 'pggan':
-          outputs = model.easy_synthesize(interpolations_batch)
-        elif gan_type == 'stylegan':
-          outputs = model.easy_synthesize(interpolations_batch, **kwargs)
-        for image in outputs['image']:
-          save_path = os.path.join(args.output_dir,
-                                   f'{sample_id:03d}_{interpolation_id:03d}.jpg')
-          cv2.imwrite(save_path, image[:, :, ::-1])
-          interpolation_id += 1
-      assert interpolation_id == args.steps
-      logger.debug(f'  Finished sample {sample_id:3d}.')
-    logger.info(f'Successfully edited {total_num} samples.')
 
+    memory_file = BytesIO()
+    zipfile_name = f"{id}.zip"
+    with ZipFile(memory_file, 'w') as zf:
+        for i, sample_id in enumerate(tqdm(range(total_num), leave=False)):
+            interpolations = linear_interpolate(latent_codes[sample_id:sample_id + 1],
+                                              boundary,
+                                              start_distance=content['min'],
+                                              end_distance=content['max'],
+                                              steps=content['steps'])
+            interpolation_id = 0
+            for interpolations_batch in model.get_batch_inputs(interpolations):
+                outputs = model.easy_synthesize(interpolations_batch, **kwargs)
+                for j, img in enumerate(outputs['image']):
+                    imname = f'{i}_{j}'
+                    formatted_img = cv2.imencode('.png', imname)
+                    print(formatted_img)
+                    zf.write(imname)
+                    interpolation_id += 1
+
+        memory_file.seek(0)
+        print(memory_file)
+        assert interpolation_id == content['steps']
+        logger.debug(f'  Finished sample {sample_id:3d}.')
+    logger.info(f'Successfully edited {total_num} samples.')
+    return send_file(memory_file, attachment_filename=zipfile_name, as_attachment=True, mimetype='application/zip')
 
 if __name__ == '__main__':
     setup()
