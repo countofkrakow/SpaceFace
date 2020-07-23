22d8ab805bf376605c3394c35a55cb0ae76bfede
Chris <chrisnakovski@gmail.com>
2020-07-23 13:48:52 -0700

    FOM working

--- first-order-model/processor.py
+++ first-order-model/processor.py
@@ -16,7 +16,8 @@ from modules.keypoint_detector import KPDetector
 from animate import normalize_kp
 from scipy.spatial import ConvexHull
 from faced import FaceDetector
-from pykalman import KalmanFilter
+from demo import make_animation
+import face_alignment
 
 log_dir = 'log'
 cfg_path = 'config/vox-adv-256.yaml'
@@ -48,93 +49,67 @@ def load_checkpoints(config_path, checkpoint_path):
 
     return generator, kp_detector
 
-def make_animation(source_image, driving_video, generator, kp_detector):
-    with torch.no_grad():
-        predictions = []
-        source = torch.tensor(source_image[np.newaxis].astype(np.float32)).permute(0, 3, 1, 2)
-        source = source.cuda()
-        driving = torch.tensor(np.array(driving_video)[np.newaxis].astype(np.float32)).permute(0, 4, 1, 2, 3)
-        kp_source = kp_detector(source)
-        kp_driving_initial = kp_detector(driving[:, :, 0])
-
-        for frame_idx in tqdm(range(driving.shape[2])):
-            driving_frame = driving[:, :, frame_idx]
-            driving_frame = driving_frame.cuda()
-            kp_driving = kp_detector(driving_frame)
-            kp_norm = normalize_kp(kp_source=kp_source, kp_driving=kp_driving,
-                                   kp_driving_initial=kp_driving_initial, use_relative_movement=True,
-                                   use_relative_jacobian=True, adapt_movement_scale=True)
-            out = generator(source, kp_source=kp_source, kp_driving=kp_norm)
-
-            predictions.append(np.transpose(out['prediction'].data.cpu().numpy(), [0, 2, 3, 1])[0])
-    return predictions
-
-def crop_video(path):
+def crop_video(path, frame_num, driving_video, crop_scale=2.5):
     face_detector = FaceDetector()
 
     cap = cv2.VideoCapture(path)
-
+    fps = cap.get(cv2.CAP_PROP_FPS) # float
     width = cap.get(cv2.CAP_PROP_FRAME_WIDTH)   # float
     height = cap.get(cv2.CAP_PROP_FRAME_HEIGHT) # float
-    fps = cap.get(cv2.CAP_PROP_FPS) # float
-
-    fourcc = cv2.VideoWriter_fourcc(*'MJPG')
-    out = cv2.VideoWriter("output.avi",fourcc, fps, (int(width), int(height)))
-
-    bbox_video = []
-    ret, frame = cap.read()
-    while ret:
-        rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-        bboxes = face_detector.predict(rgb_frame, 0.9)
-        if len(bboxes) != 0:
-            main_face = max(bboxes, key=lambda x: x[4])
-            bbox_video.append([main_face[0], main_face[1], main_face[2], main_face[3]])
-
-            print("Bounding boxes:")
-            print(bboxes)
-            print(f"main face: {main_face}")
-        else:
-            print("No bounding boxes found")
-            # TODO enlarge bounding box
-            bbox_video.append([0, 0, 0, 0])
-
-        ret, frame = cap.read()
-
-
-    # When everything done, release the capture
     cap.release()
-    kf = KalmanFilter(initial_state_mean=0, n_dim_obs=4)
-    smoothed = kf.em(bbox_video).smooth(bbox_video)
-    smoothed = [[smoothed[i][j] for j in range(len(smoothed[0]))] for i in range(len(smoothed))]
-    print("bboxes")
-    print(bbox_video)
-    print("smoothed")
-    print(smoothed)
-    print("done")
+
+    bboxes = face_detector.predict(driving_video[frame_num])
+    x, y, w, h, _ = max(bboxes, key=lambda x: x[4])
+
+    new_x = x - crop_scale * w // 2
+    new_y = y - crop_scale * w // 2
+    start_x = int(max(new_x, 0))
+    start_y = int(max(new_y, 0))
+    end_x = int(min(width - 1, start_x + crop_scale * w))
+    end_y = int(min(height - 1, start_y + crop_scale * w))
+    cropped_video = [frame[start_y:end_y, start_x:end_x] for frame in driving_video]
+    return cropped_video
+
+
+def find_best_frame(source, driving, cpu=False):
+    import face_alignment
+
+    def normalize_kp(kp):
+        kp = kp - kp.mean(axis=0, keepdims=True)
+        area = ConvexHull(kp[:, :2]).volume
+        area = np.sqrt(area)
+        kp[:, :2] = kp[:, :2] / area
+        return kp
+
+    fa = face_alignment.FaceAlignment(face_alignment.LandmarksType._2D, flip_input=True,
+                                      device='cpu' if cpu else 'cuda')
+    kp_source = fa.get_landmarks(255 * source)[0]
+    kp_source = normalize_kp(kp_source)
+    norm  = float('inf')
+    frame_num = 0
+    for i, image in tqdm(enumerate(driving)):
+        kp_driving = fa.get_landmarks(255 * image)[0]
+        kp_driving = normalize_kp(kp_driving)
+        new_norm = (np.abs(kp_source - kp_driving) ** 2).sum()
+        if new_norm < norm:
+            norm = new_norm
+            frame_num = i
+    return frame_num
 
 if __name__ == '__main__':
     SRC_IMG = os.environ['img']
     SRC_VIDEO = os.environ['video']
     RESULT_VIDEO = 'result.mp4'
     OUT_BUCKET = 'spaceface-out'
-    crop_args = {
-        'image_shape': (256, 256),
-        'increase': 0.1,
-        'iou_with_initial': 0.25,
-        'inp': SRC_VIDEO,
-        'min_frames': 150,
-        'cpu': True
-    }
+    crop_scale = 2.5
 
     with open(cfg_path) as f:
         cfg = yaml.load(f)
     os.makedirs(log_dir, exist_ok=True)
 
-    # load source image and video
-    # TODO change to cv2 imread
     source_image = imageio.imread(SRC_IMG)
     reader = imageio.get_reader(SRC_VIDEO)
-    fps = reader.get_meta_data()['fps']
+    fps = int(reader.get_meta_data()['fps'])
     driving_video = []
     try:
         for im in reader:
@@ -143,16 +118,28 @@ if __name__ == '__main__':
         pass
     reader.close()
 
-    crop_video(SRC_VIDEO)
+
+    bf = find_best_frame(source_image, driving_video)
+    driving_video = crop_video(SRC_VIDEO, bf, driving_video)
 
     # Resize input
     source_image = resize(source_image, (256, 256))[..., :3]
-    driving_video = [resize(frame, (256, 256))[..., :3] for frame in aligned_video]
+    driving_video = [resize(frame, (256, 256))[..., :3] for frame in driving_video]
+
     print("c")
     generator, kp_detector = load_checkpoints(config_path=cfg_path, checkpoint_path=model_path)
-    print("b")
-    predictions = make_animation(source_image, driving_video, generator, kp_detector)
-    print("a")
+
+    predictions = make_animation(source_image, driving_video, generator, kp_detector, relative=True)
+
+    res_fname = 'result.mp4'
+    imageio.mimsave(res_fname, [img_as_ubyte(frame) for frame in predictions], fps=fps)
+    fname = 'crop.mp4'
+    writer = imageio.get_writer(fname, fps=fps)
+    for img in driving_video:
+        writer.append_data(img)
+
+    writer.close()
     s3 = boto3.client('s3')
-    imageio.mimsave(RESULT_VIDEO, [img_as_ubyte(frame) for frame in predictions], fps=fps)
-    s3.upload_file(RESULT_VIDEO, OUT_BUCKET, RESULT_VIDEO)
+    s3.upload_file(res_fname, OUT_BUCKET, res_fname)
+    # s3.upload_file(RESULT_VIDEO, OUT_BUCKET, RESULT_VIDEO)
+    s3.upload_file(fname, OUT_BUCKET, fname)
