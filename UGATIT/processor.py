c4e78f5d2e6a8c8fcdbd057a9460c7a830be163a
Chris <chrisnakovski@gmail.com>
2020-08-05 15:21:00 -0700

    more ugatit dependencies

--- UGATIT/processor.py
+++ UGATIT/processor.py
@@ -1,7 +1,42 @@
 from UGATIT import UGATIT
 import os
+import tensorflow as tf
 
 images_bucket = 'spaceface-images'
+
+args = {
+    "phase": "train",
+    "checkpoint_dir": 0,
+    "result_dir": 0,
+    "log_dir": 0,
+    "dataset": 0,
+    "augment_flag": 0,
+    "epoch": 0,
+    "iteration": 0,
+    "decay_flag": 0,
+    "decay_epoch": 0,
+    "gan_type": 0,
+    "batch_size": 0,
+    "print_freq": 0,
+    "save_freq": 0,
+    "lr": 0,
+    "ch": 0,
+    "adv_weight": 0,
+    "cycle_weight": 0,
+    "identity_weight": 0,
+    "cam_weight": 0,
+    "GP_ld": 0,
+    "smoothing": 0,
+    "n_res": 0,
+    "n_dis": 0,
+    "n_critic": 0,
+    "sn": 0,
+    "img_size": 0,
+    "img_ch": 0,
+    "sample_dir": 0,
+    "model_dir": 0
+
+}
 
 def main():
     img_id = os.environ['id']
