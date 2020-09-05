25c3e5600bbffcb7f1896e41e1dccd82af196c61
name <email>
2020-09-04 23:47:14 -0700

    FOM improvements.
    Improved face cropping/tracking.
    Works with any size video without unlimited ram.
    Copies audio from source video.

--- first-order-model/processor.py
+++ first-order-model/processor.py
@@ -2,12 +2,12 @@ import matplotlib
 matplotlib.use('Agg')
 import os, sys
 import cv2
-import yaml
-from tqdm import tqdm
-import boto3
 import imageio
 import numpy as np
 from skimage.transform import resize
+import yaml
+from tqdm import tqdm
+import boto3
 from skimage import img_as_ubyte
 import torch
 from sync_batchnorm import DataParallelWithCallback
@@ -15,11 +15,15 @@ from modules.generator import OcclusionAwareGenerator
 from modules.keypoint_detector import KPDetector
 from animate import normalize_kp
 from scipy.spatial import ConvexHull
-from faced import FaceDetector
 from demo import make_animation
+from faced import FaceDetector
+import moviepy.editor as moviepy
 
 cfg_path = 'config/vox-adv-256.yaml'
 model_path = 'vox-adv-cpk.pth.tar'
+
+# This is the size of images before sending them to the neural networks.
+PROCESSING_IMAGE_SIZE = (256, 256)
 
 def load_checkpoints(config_path, checkpoint_path):
 
@@ -47,115 +51,123 @@ def load_checkpoints(config_path, checkpoint_path):
 
     return generator, kp_detector
 
-def crop_video(path, driving_video, face_detector, crop_scale=3):
 
-    cap = cv2.VideoCapture(path)
-    fps = cap.get(cv2.CAP_PROP_FPS) # float
-    width = cap.get(cv2.CAP_PROP_FRAME_WIDTH)   # float
-    height = cap.get(cv2.CAP_PROP_FRAME_HEIGHT) # float
-    cap.release()
-
-    best_r = 0
-    best_frame = None
-    x, y, w, h = 0, 0, 0, 0
-    for frame in driving_video:
-        bboxes = face_detector.predict(frame)
-        if len(bboxes) > 0:
-            _x, _y, _w, _h, r = max(bboxes, key=lambda x: x[4])
-            if r > best_r:
-                best_r = r
-                best_frame = frame
-                x, y, w, h = _x, _y, _w, _h
-
-    new_x = x - crop_scale * w // 2
-    new_y = y - crop_scale * w // 2
-    start_x = int(max(new_x, 0))
-    start_y = int(max(new_y, 0))
-    end_x = int(min(width - 1, start_x + crop_scale * w))
-    end_y = int(min(height - 1, start_y + crop_scale * w))
-    cropped_video = [frame[start_y:end_y, start_x:end_x] for frame in driving_video]
-    return cropped_video
+def fix_aspect_ratio(width, height, desired_aspect_ratio):
+  if (height * desired_aspect_ratio > width):
+    return (int(height * desired_aspect_ratio), height)
+  return (width, int(width / desired_aspect_ratio))
 
 
-def find_best_frame(source, driving, face_detector, cpu=False):
-    def normalize_kp(kp):
-        kp = kp - kp.mean(axis=0, keepdims=True)
-        area = ConvexHull(kp[:, :2]).volume
-        area = np.sqrt(area)
-        kp[:, :2] = kp[:, :2] / area
-        return kp
+def clip(x, minX, maxX):
+  return min(max(x, minX), maxX)
 
-    # todo REPLACE FACE_ALIGNMENT
-    bboxes, idx = [], 0
-    while len(bboxes) < 0:
-        frame = driving[idx]
-        bboxes = face_detector.predict(frame)
-        idx += 1
-        x, y, w, h, _ = max(bboxes, key=lambda x: x[4])
+def constrain_new_crop(prev_crop, new_crop, unit):
+  # Makes sure the new crop does is not too far from the old one.
+  return [int(clip(prev_crop[i] * 0.95 + new_crop[i] * 0.05, prev_crop[i] - unit, prev_crop[i] + unit)) for i in range(4)]
 
-    kp_source = fa.get_landmarks(255 * source)
 
-    # TODO check for none
-    kp_source = kp_source[0]
-    kp_source = normalize_kp(kp_source)
-    norm  = float('inf')
-    frame_num = 0
-    for i, image in tqdm(enumerate(driving)):
-        kp_driving = fa.get_landmarks(255 * image)[0]
-        kp_driving = normalize_kp(kp_driving)
-        new_norm = (np.abs(kp_source - kp_driving) ** 2).sum()
-        if new_norm < norm:
-            norm = new_norm
-            frame_num = i
-    return frame_num
+def prepare_frame(frame, face_detector, previous_crop, desired_aspect, crop_scale=1.5):
+  # Crops & formats the frame to the right size for processing. Returns the crop.
+  (height, width) = frame.shape[0:2]
+
+  bboxes = face_detector.predict(frame)
+  current_crop = previous_crop
+  if len(bboxes) > 0:
+    x, y, w, h, _ = max(bboxes, key=lambda x: x[4])
+    w, h = fix_aspect_ratio(w, h, desired_aspect)
+
+    x -= int(w * crop_scale / 2)
+    y -= int(h * crop_scale / 2)
+    w = int(w * crop_scale)
+    h = int(h * crop_scale)
+
+    new_crop = [max(x, 0), max(y, 0),
+                min(width, x + w + 1),
+                min(height, y + h + 1)]
+
+    if current_crop:
+      # Smooth the new crop.
+      # TODO: This logic messes up aspect ratio. It should not.
+      current_crop = constrain_new_crop(current_crop, new_crop, min(width, height) / 100)
+    else:
+      current_crop = new_crop
+
+  if current_crop is None:
+    print("No face in video yet, skipping frame.")
+    return None, None
+
+  cropped_frame = frame[current_crop[1]:current_crop[3], current_crop[0]:current_crop[2]]
+  scaled_frame = resize(cropped_frame, PROCESSING_IMAGE_SIZE)[..., :3]
+
+  return scaled_frame, current_crop
+
 
 if __name__ == '__main__':
-    # filename
-    SRC_VIDEO = os.environ['video']
-
-    # filename
-    SRC_IMG = os.environ['img']
+    SRC_VIDEO = os.environ['VIDEO_FILE']
+    SRC_IMG = os.environ['IMAGE_FILE']
 
     # id
-    RESULT_VIDEO = os.environ['result']
-    CROP_VIDEO = f'crop_{RESULT_VIDEO}'
-    OUT_BUCKET = 'spaceface-out'
-    SRC_BUCKET = 'spaceface-images'
+    RESULT_VIDEO = os.environ['RESULT_FILE']
+    SRC_BUCKET = os.environ['INPUT_BUCKET']
+    OUT_BUCKET = os.environ['OUTPUT_BUCKET']
 
-    crop_scale = 2.5
+
+    # This many frames will be processed at once:
+    FRAME_BATCH_SIZE = int(os.environ['FRAME_BATCH_SIZE']) # 200 is ~3 GB of RAM
 
     s3 = boto3.client('s3')
     s3.download_file(SRC_BUCKET, SRC_IMG, SRC_IMG)
     s3.download_file(SRC_BUCKET, SRC_VIDEO, SRC_VIDEO)
 
-    source_image = imageio.imread(SRC_IMG)
-    reader = imageio.get_reader(SRC_VIDEO)
-    fps = int(reader.get_meta_data()['fps'])
-    driving_video = []
-    try:
-        for im in reader:
-            driving_video.append(im)
-    except RuntimeError:
-        pass
-    reader.close()
-
-    fd = FaceDetector()
-    driving_video = crop_video(SRC_VIDEO, driving_video, fd)
-
-    # Resize input
-    source_image = resize(source_image, (256, 256))[..., :3]
-    driving_video = [resize(frame, (256, 256))[..., :3] for frame in driving_video]
+    face_detector = FaceDetector()
+    previous_crop = None
 
     generator, kp_detector = load_checkpoints(config_path=cfg_path, checkpoint_path=model_path)
 
-    predictions = make_animation(source_image, driving_video, generator, kp_detector, relative=True)
+    source_image = imageio.imread(SRC_IMG)
+    (source_height, source_width) = source_image.shape[0:2]
+    source_image = resize(source_image, PROCESSING_IMAGE_SIZE)[..., :3]
 
-    imageio.mimsave(RESULT_VIDEO, [img_as_ubyte(frame) for frame in predictions], fps=fps)
+    video_reader = imageio.get_reader(SRC_VIDEO)
+    fps = int(video_reader.get_meta_data()['fps'])
+    temp_video_file = "no_sound_" + RESULT_VIDEO
+    video_writer = imageio.get_writer(temp_video_file, fps=fps)
 
-    writer = imageio.get_writer(CROP_VIDEO, fps=fps)
-    for img in driving_video:
-        writer.append_data(img)
+    # Debug only:
+    # cropped_input_writer = imageio.get_writer('cropped_' + RESULT_VIDEO, fps=fps)
 
-    writer.close()
-    s3.upload_file(CROP_VIDEO, OUT_BUCKET, CROP_VIDEO)
+    frames_left = len(video_reader)
+    frames_skipped = 0
+
+    while frames_left > 0:
+      frames = []
+      for _ in range(min(FRAME_BATCH_SIZE, frames_left)):
+        frame, previous_crop = prepare_frame(video_reader.get_next_data(), face_detector, previous_crop, source_width / source_height)
+        frames_left -= 1
+        if frame is None:
+          # This is the case where we don't see a face at the start of a video. We just skip those frames.
+          frames_skipped += 1
+          continue
+        # Debug only:
+        # cropped_input_writer.append_data(frame)
+        frames.append(frame)
+
+      if len(frames) > 0:
+        animated_image = make_animation(source_image, frames, generator, kp_detector, relative=True)
+        for animated_frame in animated_image:
+          video_writer.append_data(img_as_ubyte(animated_frame))
+
+    video_reader.close()
+    video_writer.close()
+    # Debug only:
+    # cropped_input_writer.close()
+
+    # Add sound:
+    animated_video = moviepy.VideoFileClip(temp_video_file)
+    original_video = moviepy.VideoFileClip(SRC_VIDEO)
+
+    if original_video.audio is not None:
+      animated_video = animated_video.set_audio(original_video.audio.subclip(frames_skipped / fps))
+    animated_video.write_videofile(RESULT_VIDEO)
+
     s3.upload_file(RESULT_VIDEO, OUT_BUCKET, RESULT_VIDEO)
